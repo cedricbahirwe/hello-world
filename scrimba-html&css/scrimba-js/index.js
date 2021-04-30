b92549b1a1b77300f01f86f8e552ffaf0956e844
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-30 08:25:55 +0200

    Functions

--- scrimba-html&css/scrimba-js/index.js
+++ scrimba-html&css/scrimba-js/index.js
@@ -1,24 +1,22 @@
-
-
-let totalUpdates = 0;
-
-
-let value  = 044;
-function addList() {
-
-    if (value > 10) {
-        value = 'null'
-        addList
-        return  value
-    } else if (value == 'null') {
-        return 'Infinitelyt low number'
-    } else {
-        return
-    }
-
+function getUserCredentials(firstName, lastName, email) {
+    return firstName + lastName + email
 }
 
-let val  = typeof 10;
+const cartItems = [
+    { quantity: 1, price: 5 },
+    { quantity: 3, price: 4 },
+    { quantity: 10, price: 1}
+];
+
+function getPreTaxTotal(items) {
+    let total = 0;
+
+    for (item of cartItems) {
+        total = total + item.price * item.quantity;
+    }
+    return total
+}
+// console.log(getUserCredentials('cedric', 10+23,"98"));
 
 
-console.log(addList())
+console.log(getPreTaxTotal(cartItems));

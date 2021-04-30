bf2b6bc808629e97f00fdb55161153b1d8fa1ae7
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-30 08:07:51 +0200

    Different Sort of Loops

--- scrimba-html&css/scrimba-js/index.js
+++ scrimba-html&css/scrimba-js/index.js
@@ -1,1 +1,24 @@
-let products = ['toilet paper', 'bottled water', 'sanitizer']
+
+
+let totalUpdates = 0;
+
+
+let value  = 044;
+function addList() {
+
+    if (value > 10) {
+        value = 'null'
+        addList
+        return  value
+    } else if (value == 'null') {
+        return 'Infinitelyt low number'
+    } else {
+        return
+    }
+
+}
+
+let val  = typeof 10;
+
+
+console.log(addList())
